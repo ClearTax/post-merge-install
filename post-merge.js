3fe69e24a0b99f7c50b27199ac10e9fa8efb3ba9
Aditi Mohanty <rheaditi@users.noreply.github.com>
2019-09-17 15:23:05 +0530

    run in other directories post merge

--- post-merge.js
+++ post-merge.js
@@ -1,24 +1,37 @@
 #!/usr/bin/env node
-const { execSync, spawn } = require('child_process')
+const { execSync, spawn } = require('child_process');
+const { dirname, resolve } = require('path');
 const chalk = require('chalk');
+const { getDirectoriesToInstall, spawnProcess } = require('./utils');
+
+const workingDirectory = process.cwd();
 
 (async () => {
-  const diffTree = execSync('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').toString()
+  const diffTree = execSync('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').toString();
+  const packageFiles = getPackageFiles(diffTree);
 
-  const filesChanged = (diffTree || '').split('\n').filter(Boolean)
-  const needsInstall =
-    !process.env.CI && filesChanged.some(file => file === 'package.json' || file === 'package-lock.json')
+  const filesChanged = (diffTree || '').split('\n').filter(Boolean);
+  const needsInstall = !process.env.CI && packageFiles.length;
 
   if (needsInstall) {
     console.log(
       chalk.yellow(`
-        Detected changes in "${chalk.bold('package.json')}" or "${chalk.bold('package-lock.json')}".
-        Running "${chalk.bold('npm install')}" for you ..
-      `)
-    )
-    const npmInstall = spawn(`npm`, ['install'], { stdio: 'inherit' })
-    npmInstall.on('close', returnCode => {
-      process.exit(returnCode)
-    })
+Detected changes in "${chalk.bold('package.json')}" or "${chalk.bold('package-lock.json')}".
+Running "${chalk.bold('npm install')}" in the corresponding directories..
+\n\n`)
+    );
+
+    const directories = getDirectoriesToInstall(filesChanged);
+
+    try {
+      for (const directory of directories) {
+        await spawn(`npm`, ['install'], {
+            cwd: resolve(workingDirectory, directory),
+            stdio: 'inherit' });
+      }
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   }
 })()
