5a5ce7571d7c1ffe44d8627486ee8494796e6057
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-09-19 17:17:11 +0530

    lint fix

--- post-merge.js
+++ post-merge.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
-const { execSync, spawn } = require('child_process');
-const { dirname, resolve } = require('path');
-const chalk = require('chalk');
+const { execSync } = require('child_process')
+const { resolve } = require('path')
+const chalk = require('chalk')
 const { getDirectoriesToInstall, getPackageFiles } = require('./utils');
 
 (async () => {
-  const diffTree = execSync('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').toString().trim();
-  const gitRoot = execSync('git rev-parse --show-toplevel').toString().trim();
+  const diffTree = execSync('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').toString().trim()
+  const gitRoot = execSync('git rev-parse --show-toplevel').toString().trim()
 
-  const changedPackageFiles = getPackageFiles(diffTree);
-  const needsInstall = !process.env.CI && changedPackageFiles.length;
+  const changedPackageFiles = getPackageFiles(diffTree)
+  const needsInstall = !process.env.CI && changedPackageFiles.length
 
   if (needsInstall) {
     console.log(
       chalk.yellow(`
 Detected changes in "${chalk.bold('package.json')}" and/or "${chalk.bold('package-lock.json')}".
 Running "${chalk.bold('npm install')}" in the corresponding directories..\n`)
-    );
+    )
 
-    const directories = getDirectoriesToInstall(changedPackageFiles);
+    const directories = getDirectoriesToInstall(changedPackageFiles)
 
     try {
       for (const directory of directories) {
-        const installDirectory = resolve(gitRoot, directory);
-        console.log(chalk.yellow.dim(`‣ ${chalk.underline(installDirectory)}\n`));
+        const installDirectory = resolve(gitRoot, directory)
+        console.log(chalk.yellow.dim(`‣ ${chalk.underline(installDirectory)}\n`))
 
-        execSync(`npm install`, {
+        execSync('npm install', {
           stdio: 'inherit',
-          cwd: installDirectory,
-        });
+          cwd: installDirectory
+        })
       }
     } catch (e) {
-      console.error(`error running 'npm install':`, e);
-      process.exit(1);
+      console.error('error running \'npm install\':', e)
+      process.exit(1)
     }
   }
 })()
